a49a75b3e2842ae25e57be0c5cc178124f344163
Saykhon <islomkhon1243@gmail.com>
2023-03-21 16:58:48 +0600

    Delete user

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'; 
+import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
+
+mongoose
+.connect('mongodb+srv://islomkhon1243:islomkhon1243@cluster0.alcnfvk.mongodb.net/futurum?retryWrites=true&w=majority')
+.then(() => console.log('DB Ok'))
+.catch((err) => console.log('DB error', err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', UserController.login);
+app.post('/auth/register', registerValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.listen(3000, err => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
